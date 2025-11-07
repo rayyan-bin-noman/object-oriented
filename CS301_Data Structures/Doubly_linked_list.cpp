a5c35b5edba8c5ddc9eff7e6736c878ac4861450
rayyan-bin-noman <rayyanbinoman@gmail.com>
2025-11-07 18:56:39 +0500

    doubly linked list final

--- CS301_Data Structures/Doubly_linked_list.cpp
+++ CS301_Data Structures/Doubly_linked_list.cpp
@@ -1,222 +1,198 @@
-#include <iostream>      // include header for input/output
-using namespace std;     // allows use of cout, cin, etc. without std::
+#include <iostream>
+using namespace std;
 
-
-//  Node Class - represents a single element in the list
-
-class Node 
-{
+class Node {
 public:
-    int data;            // stores the data value of the node
-    Node *next_addr;     // pointer to the next node in the list
-    Node *pre_addr;     // pointer to the previous node in the list
+    int data;
+    Node* next_addr;
+    Node* pre_addr;
+    Node(int v) : data(v), next_addr(nullptr), pre_addr(nullptr) {}
 };
 
-
-//  Linked_list Class - manages all nodes
-
-class Linked_list 
-{
+class Linked_list {
 public:
-    Node *head = NULL;   // pointer to the first node in the list (NULL means empty list)
-    Node *tail = NULL;   // pointer to the last node in the list (NULL means empty list)
+    Node* head = nullptr;
+    Node* tail = nullptr;
 
-    
-    // Function: setdata()
-    // Purpose: Insert a new node at the end
-    
-    void setdata() 
-    {
+    // Insert node at the end
+    void setdata() {
         int n;
         cout << "\nPlease Enter Value: ";
-        cin >> n;                            // take value from user
+        cin >> n;
 
-        Node *new_node = new Node;           // dynamically create a new node using 'new'
-        new_node->data = n;                  // store user's value in node
-        new_node->next_addr = NULL;          // since it’s the last node for now
+        Node* new_node = new Node(n);
 
-        if (head == NULL)                    // if the list is empty
-        {                 
-            head = new_node;                 // make this node the first node (head)
-        } 
-        else                                 // otherwise, find the last node
-        {                           
-            Node *ptr = head;                // start from the first node
-            while (ptr->next_addr != NULL) { // move till last node (where next_addr is NULL)
-                ptr = ptr->next_addr;
-            }
-            ptr->next_addr = new_node;       // connect last node to new node
+        if (head == nullptr) {
+            // first node in the list
+            head = tail = new_node;
+        } else {
+            // append to tail
+            tail->next_addr = new_node;
+            new_node->pre_addr = tail;
+            tail = new_node;
         }
         cout << "\nNew node inserted successfully....";
     }
 
-    
-    // Function: count()
-    // Purpose: Count total number of nodes
-    
-    void count() 
-    {
-        if (head == NULL)                    // if list is empty
-        {                 
+    // Return number of nodes (does NOT print)
+    int count() const {
+        int c = 0;
+        Node* ptr = head;
+        while (ptr != nullptr) {
+            ++c;
+            ptr = ptr->next_addr;
+        }
+        return c;
+    }
+
+    // Search for a value and print its position (1-based)
+    void search() const {
+        if (head == nullptr) {
             cout << "\nLinked List is Empty....";
-        } 
-        else 
-        {
-            int n = 0;                       // counter for nodes
-            Node *ptr = head;                // start from first node
-            while (ptr != NULL) {            // continue till last node
-                n++;                         // increase count
-                ptr = ptr->next_addr;        // move to next node
+            return;
+        }
+        int n, position = 1;
+        bool found = false;
+        cout << "\nPlease Enter the value to be searched: ";
+        cin >> n;
+
+        Node* ptr = head;
+        while (ptr != nullptr) {
+            if (ptr->data == n) {
+                cout << "\nValue found at node " << position << ".";
+                found = true;
+                break;
             }
-            cout << "\nTotal Nodes are: " << n;
+            ptr = ptr->next_addr;
+            ++position;
+        }
+        if (!found) {
+            cout << "\nValue doesn't exist in Linked list....";
         }
     }
 
-    
-    // Function: search()
-    // Purpose: Search for a specific value
-    
-    void search() 
-    {
-        if (head == NULL)                     // check if list empty
-        {                 
+    // Display nodes forward
+    void getdata() const {
+        if (head == nullptr) {
             cout << "\nLinked List is Empty....";
-        } else 
-        {
-            int n, found = 0, position = 1;
-            cout << "\nPlease Enter the value to be searched: ";
-            cin >> n;                        // take value to search
-            Node *ptr = head;                // start from first node
-            while (ptr != NULL)              // traverse through list
-            {            
-                if (n == ptr->data) {        // if value found
-                    found = 1;
-                    cout << "\n✅ Value found at node " << position << ".";
-                    break;                   // stop searching
+            return;
+        }
+        Node* ptr = head;
+        int idx = 1;
+        cout << "\nNodes in Linked List:\n";
+        while (ptr != nullptr) {
+            cout << idx << " : " << ptr->data << endl;
+            ptr = ptr->next_addr;
+            ++idx;
+        }
+    }
+
+    // Display nodes backward using 'tail'
+    void getdata_back() const {
+        if (head == nullptr) {
+            cout << "\nLinked List is Empty....";
+            return;
+        }
+        Node* ptr = tail;
+        int idx = count(); // count() does not print now
+        cout << "\nNodes in Linked List (In Reverse Order):\n";
+        while (ptr != nullptr) {
+            cout << idx << " : " << ptr->data << endl;
+            ptr = ptr->pre_addr;
+            --idx;
+        }
+    }
+
+    // Update first occurrence of a value
+    void update() {
+        if (head == nullptr) {
+            cout << "\nLinked List is Empty....";
+            return;
+        }
+        getdata(); // optional: show list before update
+        int oldVal;
+        cout << "\nPlease Enter the value for Updation: ";
+        cin >> oldVal;
+
+        Node* ptr = head;
+        int position = 1;
+        bool found = false;
+        while (ptr != nullptr) {
+            if (ptr->data == oldVal) {
+                cout << "\nPlease enter new value: ";
+                cin >> ptr->data;
+                cout << "\nValue has been updated at node " << position << ".";
+                found = true;
+                break;
+            }
+            ptr = ptr->next_addr;
+            ++position;
+        }
+        if (!found) {
+            cout << "\nValue doesn't exist in Linked list....";
+        }
+    }
+
+    // Delete first occurrence of value n
+    void del() {
+        if (head == nullptr) {
+            cout << "\nLinked List is Empty....";
+            return;
+        }
+
+        getdata(); // optional: show list before deletion
+
+        int n;
+        cout << "\nPlease Enter the Node's value to delete: ";
+        cin >> n;
+
+        Node* ptr = head;
+        int position = 1;
+        bool found = false;
+
+        while (ptr != nullptr) {
+            if (ptr->data == n) {
+                // Link previous node to next node (if prev exists)
+                if (ptr->pre_addr != nullptr) {
+                    ptr->pre_addr->next_addr = ptr->next_addr;
+                } else {
+                    // deleting head
+                    head = ptr->next_addr;
                 }
-                ptr = ptr->next_addr;        // move to next node
-                position++;                  // increase position
-            }
-            if (found == 0)                  // if not found
-            {               
-                cout << "\nValue doesn't exist in Linked list....";
-            }
-        }
-    }
 
-       
-    // Function: getdata()
-    // Purpose: Display all node values
-    
-    void getdata()
-    {
-        if (head == NULL)
-        {                  // check if empty
-            cout << "\nLinked List is Empty....";
-        }
-        else
-        {
-            Node *ptr = head;                // start from first node
-            int n = 1;                       // for numbering nodes
-            cout << "\nNodes in Linked List:\n";
-            while (ptr != NULL) {            // traverse till end
-                cout << n << " : " << ptr->data << endl;
-                ptr = ptr->next_addr;        // go to next node
-                n++;                         // increase index
-            }
-        }
-    }
- 
-    // Function: update()
-    // Purpose: Update a node’s value
-    
-    void update()
-    {
-        if (head == NULL)                   // check if list is empty
-        {   
-            cout << "\nLinked List is Empty....";
-        } else
-        {
-            getdata();
-            int n, found = 0;
-            cout << "\nPlease Enter the value for Updation: ";
-            cin >> n;                      // take old value to find
-            Node *ptr = head;              // start from first node
-            int position = 1;              // to track node number
-
-            // traverse the list
-            while (ptr != NULL)
-            {
-                if (n == ptr->data)        // value found
-                {     
-                    cout << "\nPlease enter new value: ";
-                    cin >> ptr->data;      // update node value
-                    found = 1;
-                    cout << "\n✅ Value has been updated at node " << position << ".";
-                    break;                 // stop searching
+                // Link next node to previous node (if next exists)
+                if (ptr->next_addr != nullptr) {
+                    ptr->next_addr->pre_addr = ptr->pre_addr;
+                } else {
+                    // deleting tail
+                    tail = ptr->pre_addr;
                 }
-                ptr = ptr->next_addr;      // move to next node
-                position++;                // increment node counter
-            }
 
-            if (found == 0)              // value not found
-            {              
-                cout << "\nValue doesn't exist in Linked list....";
+                delete ptr;
+                cout << "\nNode deleted successfully (was node " << position << ").";
+                found = true;
+                break;
             }
+            ptr = ptr->next_addr;
+            ++position;
+        }
+
+        if (!found) {
+            cout << "\nValue doesn't exist in Linked list....";
         }
     }
 
-    
-    // Function: del()
-    // Purpose: Delete a specific node
-    
-    void del()
-    {
-        if (head == NULL)                     // check if empty
-        {                 
-            cout << "\nLinked List is Empty....";
+    // optionally add destructor to free memory on program exit
+    ~Linked_list() {
+        Node* curr = head;
+        while (curr != nullptr) {
+            Node* next = curr->next_addr;
+            delete curr;
+            curr = next;
         }
-        else
-        {
-            getdata();
-            int n, found = 0, position = 1;
-            cout << "\nPlease Enter the Node's value to delete: ";
-            cin >> n;
-
-            Node *ptr = head;                // pointer to current node
-            Node *prev = NULL;               // pointer to previous node
-
-            // traverse the list
-            while (ptr != NULL)
-            {
-                if (ptr->data == n)
-                {        // value found
-                    found = 1;
-                    if (prev == NULL)        // deleting first node
-                    {     
-                        head = ptr->next_addr;
-                    } 
-                    else                      // deleting middle or last node
-                    {               
-                        prev->next_addr = ptr->next_addr;
-                    }
-                    delete ptr;              // free memory
-                    cout << "\nNode deleted successfully (was node " << position << ").";
-                    break;
-                }
-                prev = ptr;                  // move prev to current
-                ptr = ptr->next_addr;        // move current to next1
-                position++;                  // update node counter
-            }
-
-            if (found == 0)                  // if not found
-            {              
-                cout << "\nValue doesn't exist in Linked list....";
-            }
-        }
+        head = tail = nullptr;
     }
 };
-
 
 //  MAIN FUNCTION - Menu driven program
 
@@ -233,8 +209,9 @@ int main()
         cout << "\n3 : Search Node";
         cout << "\n4 : Update Node";
         cout << "\n5 : Delete Node";
-        cout << "\n6 : Show Nodes";
-        cout << "\n7 : Exit";
+        cout << "\n6 : Show Forward Nodes";
+        cout << "\n7 : Show Backward Nodes";
+        cout << "\n8 : Exit";
         cout << "\n\nEnter your choice: ";
         cin >> choice;                       // input user choice
 
@@ -257,13 +234,17 @@ int main()
             obj.del();                       // call delete function
             break;
         case 6:
-            obj.getdata();                   // call display function
+            obj.getdata();                   // call forward display function
             break;
         case 7:
+            obj.getdata_back();              // call backward display function
+            break;
+        case 8:
             exit(0);                         // exit program
         default:
             cout << "\nInvalid Choice..... Please try again.\n";
         }
     }
-    return 0;                                // program ends
+    
+    return 0;                                // program ends
 }
